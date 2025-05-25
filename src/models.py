33881faacd9525d9b2d7dcc5fb601faae47c02b2
Azhou2023 <anz008@ucsd.edu>
2025-05-24 21:47:45 -0700

    revert"

--- src/models.py
+++ src/models.py
@@ -174,7 +174,7 @@ class OutConv(nn.Module):
         return self.conv(x)
 
 class UNet(nn.Module):
-    def __init__(self, n_input_channels, n_output_channels, dropout_rate=0, bilinear=False):
+    def __init__(self, n_input_channels, n_output_channels, dropout_rate=0.4, bilinear=False):
         super(UNet, self).__init__()
         self.n_channels = n_input_channels
         self.n_classes = n_output_channels
@@ -185,13 +185,12 @@ class UNet(nn.Module):
         self.down1 = (Down(64, 128))
         self.down2 = (Down(128, 256))
         self.down3 = (Down(256, 512))
-        self.down4 = (Down(512, 1024))
-        self.down5 = (Down(1024, 2048))
-        self.up1 = (Up(2048, 1024, bilinear))
-        self.up2 = (Up(1024, 512, bilinear))
-        self.up3 = (Up(512, 256, bilinear))
-        self.up4 = (Up(256, 128, bilinear))
-        self.up5 = (Up(128, 64, bilinear))
+        factor = 2 if bilinear else 1
+        self.down4 = (Down(512, 1024 // factor))
+        self.up1 = (Up(1024, 512 // factor, bilinear))
+        self.up2 = (Up(512, 256 // factor, bilinear))
+        self.up3 = (Up(256, 128 // factor, bilinear))
+        self.up4 = (Up(128, 64, bilinear))
         self.outc = (OutConv(64, n_output_channels))
 
     def forward(self, x):
@@ -209,14 +208,10 @@ class UNet(nn.Module):
         x5 = self.down4(x4)
         x5 = self.dropout(x5)
         
-        x6 = self.down5(x5)
-        x6 = self.dropout(x6)
-        
-        x = self.up1(x6, x5)
-        x = self.up2(x, x4)
-        x = self.up3(x, x3)
-        x = self.up4(x, x2)
-        x = self.up5(x, x1)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
         x = self.dropout(x)
         logits = self.outc(x)
         return logits
