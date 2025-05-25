2784b963eefd9fda911638cc6574d3eebb9cc9bd
Azhou2023 <anz008@ucsd.edu>
2025-05-24 20:49:59 -0700

    unet

--- src/models.py
+++ src/models.py
@@ -174,35 +174,49 @@ class OutConv(nn.Module):
         return self.conv(x)
 
 class UNet(nn.Module):
-    def __init__(self, n_input_channels, n_output_channels, dropout_rate, bilinear=False):
+    def __init__(self, n_input_channels, n_output_channels, dropout_rate=0, bilinear=False):
         super(UNet, self).__init__()
         self.n_channels = n_input_channels
         self.n_classes = n_output_channels
         self.bilinear = bilinear
 
+        self.dropout = nn.Dropout2d(dropout_rate)
         self.inc = (DoubleConv(n_input_channels, 64))
         self.down1 = (Down(64, 128))
         self.down2 = (Down(128, 256))
         self.down3 = (Down(256, 512))
-        factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.dropout = nn.Dropout2d(dropout_rate)
+        self.down4 = (Down(512, 1024))
+        self.down5 = (Down(1024, 2048))
+        self.up1 = (Up(2048, 1024, bilinear))
+        self.up2 = (Up(1024, 512, bilinear))
+        self.up3 = (Up(512, 256, bilinear))
+        self.up4 = (Up(256, 128, bilinear))
+        self.up5 = (Up(128, 64, bilinear))
         self.outc = (OutConv(64, n_output_channels))
 
     def forward(self, x):
         x1 = self.inc(x)
+        
         x2 = self.down1(x1)
+        x2 = self.dropout(x2)
+        
         x3 = self.down2(x2)
+        x3 = self.dropout(x3)
+        
         x4 = self.down3(x3)
+        x4 = self.dropout(x4)
+        
         x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        x5 = self.dropout(x5)
+        
+        x6 = self.down5(x5)
+        x6 = self.dropout(x6)
+        
+        x = self.up1(x6, x5)
+        x = self.up2(x, x4)
+        x = self.up3(x, x3)
+        x = self.up4(x, x2)
+        x = self.up5(x, x1)
         x = self.dropout(x)
         logits = self.outc(x)
         return logits
